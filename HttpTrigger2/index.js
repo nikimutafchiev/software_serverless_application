1a353ef944446c6600f4badcf80bdf3e88bb829b
Nikolay Mutafchiev <niki_mutafchiev@yahoo.com>
2024-03-20 19:11:18 +0200

    Update trigger 1 & 2

--- HttpTrigger2/index.js
+++ HttpTrigger2/index.js
@@ -1,6 +1,5 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
-
     const movie_id = req.body.movie_id, rating = req.body.rating, review = req.body.review, date = req.body.date, author = req.body.author;
     context.bindings.Rating = JSON.stringify([{
         movie_id: movie_id,
@@ -9,7 +8,8 @@ module.exports = async function (context, req) {
         date: date,
         author: author
     }]);
-    const responseMessage = `Successfully added rating for movie with id: ${movie_id}`
+    const movie_name = context.bindings.MovieName[0].name;
+    const responseMessage = `Successfully added rating for movie with name: ${movie_name}`
     context.res = {
         body: responseMessage
     };
