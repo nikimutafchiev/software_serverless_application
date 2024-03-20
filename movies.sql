937c40b7eef3b800ba4ce780b83fc390adffd627
Nikolay Mutafchiev <114503385+nikimutafchiev@users.noreply.github.com>
2024-03-20 08:08:41 +0200

    Update movies.sql

--- movies.sql
+++ movies.sql
@@ -1,7 +1,5 @@
 CREATE TABLE Movies(
 	id int PRIMARY KEY IDENTITY,
-	rating_id int,
-	FOREIGN KEY(rating) id REFERENCES [dbo].[Ratings](id),
 	name text not null,
 	year int not null,
 	genre text not null,
